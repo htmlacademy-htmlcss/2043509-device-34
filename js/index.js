b9c7ef4f9cd2706cb7dd64e28c8a4fa0aa1adafd
Vladimir-Bumagin <vl.bumagin@yandex.ru>
2022-08-12 17:08:46 +0300

    Добавляет все компоненты.

--- js/index.js
+++ js/index.js
@@ -17,9 +17,12 @@ const sliderItems = document.querySelectorAll('.slider-item');
 const sliderButtonNext = document.querySelector('.slider-button-next');
 const sliderButtonPrev = document.querySelector('.slider-button-prev');
 
+let slideIndex = 0;
+
 paginationButtons.forEach((button, index) => {
   button.addEventListener('click', (evt) => {
     evt.preventDefault();
+    slideIndex = index;
     document.querySelector('.pagination-button.is-active').classList.remove('is-active');
     document.querySelector('.slider-item.is-active').classList.remove('is-active');
     button.classList.add('is-active');
@@ -27,31 +30,57 @@ paginationButtons.forEach((button, index) => {
   });
 })
 
-let index = 0;
-
 sliderButtonNext.addEventListener('click', (evt) => {
   evt.preventDefault();
-  paginationButtons[index].classList.remove('is-active');
-  sliderItems[index].classList.remove('is-active');
-  if (index === paginationButtons.length - 1) {
-    index = 0;
+  paginationButtons[slideIndex].classList.remove('is-active');
+  sliderItems[slideIndex].classList.remove('is-active');
+  if (slideIndex === paginationButtons.length - 1) {
+    slideIndex = 0;
   } else {
-    index = index + 1;
+    slideIndex = slideIndex + 1;
   }
-  paginationButtons[index].classList.add('is-active');
-  sliderItems[index].classList.add('is-active');
+  paginationButtons[slideIndex].classList.add('is-active');
+  sliderItems[slideIndex].classList.add('is-active');
 });
 
 sliderButtonPrev.addEventListener('click', (evt) => {
   evt.preventDefault();
-  paginationButtons[index].classList.remove('is-active');
-  sliderItems[index].classList.remove('is-active');
-  if (index != 0) {
-    index = index - 1;
+  paginationButtons[slideIndex].classList.remove('is-active');
+  sliderItems[slideIndex].classList.remove('is-active');
+  if (slideIndex != 0) {
+    slideIndex = slideIndex - 1;
   } else {
-    index = paginationButtons.length - 1;
+    slideIndex = paginationButtons.length - 1;
   }
-  paginationButtons[index].classList.add('is-active');
-  sliderItems[index].classList.add('is-active');
+  paginationButtons[slideIndex].classList.add('is-active');
+  sliderItems[slideIndex].classList.add('is-active');
   index === paginationButtons[index];
+});
+
+
+const deliveryLink = document.querySelector('.delivery-link');
+const modalCloseButton = document.querySelector('.modal-close-button');
+const modal = document.querySelector('.modal-container');
+
+deliveryLink.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  modal.classList.add('is-open');
+});
+
+modalCloseButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  modal.classList.remove('is-open');
+});
+
+document.addEventListener('keydown', (evt) => {
+  if(evt.key === 'Escape') {
+    evt.preventDefault();
+    modal.classList.remove('is-open');
+  }
+});
+
+document.addEventListener('click', (evt) =>  {
+  if ( evt.target === modal) {
+   modal.classList.remove('is-open');
+  }
 });
